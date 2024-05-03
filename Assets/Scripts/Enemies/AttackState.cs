746f65dfe5655b2b7fe963238c387b6fa99a1af2
emadrigalv <emmanuel.mv99@gmail.com>
2024-05-03 18:38:10 -0500

    Enemy Attack State Updated, Audio manager Updated

--- Assets/Scripts/Enemies/AttackState.cs
+++ Assets/Scripts/Enemies/AttackState.cs
@@ -7,10 +7,17 @@ public class AttackState : State
     [Header("Animation")]
     [SerializeField] private AnimationClip _stateAnimation;
 
+    [Header("Events")]
+    [SerializeField] private StringEvent _attackSFXEvent;
+
+    [Header("Parameters")]
+    [SerializeField] private string _attackSFXName;
+
     public override void Enter()
     {
         _core.Rigidbody.velocity = Vector3.zero;
         _core.Animator.Play(_stateAnimation.name);
+        _attackSFXEvent.Invoke(_attackSFXName);
     }
 
     public override void Do()
